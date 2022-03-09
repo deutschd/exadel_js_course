cab4255d92ab4780807e5c205f30ba4370344548
deutschd <ismailovshakhzod@gmail.com>
2022-03-09 12:16:15 +0500

    homework_2_2 = var -> let

--- homework_2/homework_2_2.js
+++ homework_2/homework_2_2.js
@@ -1,5 +1,5 @@
 function validatePhoneNymber(str) {
-    var isphone = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/.test(str);
+    let isphone = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/.test(str); // var -> let
     console.log(isphone);
   }
 
